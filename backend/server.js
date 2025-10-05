5cedd790f07286aba7789cc3ae1aa232e398b2ab
Adarsh kumar <adarshpandey9910@gmail.com>
2025-10-05 15:22:29 +0530

    project initialisation-backend setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
